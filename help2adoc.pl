03722e97261aeaa276fdfb040774d15cbbfeb39a
Klas Lindfors <klas@yubico.com>
2015-07-01 12:18:15 +0200

    start of help2adoc

---
+++ help2adoc.pl
@@ -0,0 +1,136 @@
+#!/usr/bin/perl
+
+# Copyright (c) 2015 Yubico AB
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+# 
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+# 
+#     * Redistributions in binary form must reproduce the above
+#       copyright notice, this list of conditions and the following
+#       disclaimer in the documentation and/or other materials provided
+#       with the distribution.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+use strict;
+use warnings;
+
+use Getopt::Long qw/:config auto_abbrev no_ignore_case/;
+
+our $VERSION = "0.0.1";
+
+my $executable;
+my $helpSwitch = "--help";
+my $versionSwitch = "--version";
+my $verbose = 0;
+
+GetOptions("executable=s" => \$executable,
+  "help-switch=s" => \$helpSwitch,
+  "version-switch=s" => \$versionSwitch,
+  "verbose|v" => \$verbose,
+  "help|h" => \&help,
+  "version|V" => \&version,
+);
+
+die "No executable given" unless $executable;
+
+sub help {
+  print "help2adoc $VERSION\n\n";
+  print "Usage: help2adoc [OPTIONS]...\n\n";
+  print "  -e, --executable\twhat executable to extract from\n";
+  print "  --help-switch\t\twhat option to get help (default=--help)\n";
+  print "  --version-switch\twhat option to get version (default=--version)\n";
+  print "  -v, --verbose\t\tprint more information\n";
+  print "  -h, --help\t\tPrint help and exit\n";
+  print "  -V, --version\t\tPrint version and exit\n";
+  exit 0;
+}
+
+sub version {
+  print "help2adoc $VERSION\n";
+  exit 0;
+}
+
+my $name;
+my $synopsis;
+my @help;
+my $version;
+
+warn "Going to run '$executable' with '$helpSwitch' and '$versionSwitch'.\n" if $verbose;
+open(my $helpStream, "-|", "$executable $helpSwitch");
+die "Failed to run $executable with $helpSwitch" unless $helpStream;
+
+my $gotHelp = 0;
+my $gotEmpty = 0;
+while(<$helpStream>) {
+  s/^\s+|\s+$//g;
+  if(m/^Usage: ([a-zA-Z0-9-_]+) (.*)?/) {
+    warn "'$_' matched as usage" if $verbose;
+    $name = $1 if $1;
+    $synopsis = "$1 $2" if $1 and $2;
+  } elsif(m/^-/) {
+    $gotHelp++;
+    $gotEmpty = 0;
+    push @help, $_;
+  } elsif($gotHelp) {
+    my $part = pop(@help);
+    $part .= "\n$_";
+    push @help, $part;
+  }
+}
+close $helpStream;
+
+open(my $versionStream, "-|", "$executable $versionSwitch");
+die "Failed to run $executable with $versionSwitch" unless $versionStream;
+while(<$versionStream>) {
+  s/^\s+|\s+$//g;
+  if(m/^$name.*\s([a-zA-Z0-9-_\.]+)$/) {
+    $version = $1;
+  } elsif(m/^([a-zA-Z0-9-_\.]+)/) {
+    $version = $1;
+  }
+  last if $version;
+}
+close $versionStream;
+
+print "= " . uc($name) . "(1)\n";
+print ":doctype:\tmanpage\n";
+print ":man source:\t$name\n";
+print ":man manual:\t$name Manual\n\n";
+print "== NAME\n";
+print "$name - $name\n\n";
+print "== SYNOPSIS\n";
+print "$synopsis\n\n";
+print "== OPTIONS\n";
+foreach my $option (@help) {
+  my @first;
+  my $second;
+  foreach my $part (split(/\s/, $option)) {
+    if($part =~ m/^-/) {
+      push @first, $part;
+    } else {
+      last;
+    }
+  }
+  $second = $option;
+  $second =~ s/-[a-zA-Z0-9-_=]+//g;
+  $second =~ s/^,\s*//g;
+
+  print "*" . join(" ", @first) . "*::\n";
+  print "$second\n\n";
+}
